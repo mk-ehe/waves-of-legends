31ab1cecbe91becb19ca9003a5407c836ee8aac1
Mateusz <kontakt.mateusz.kudas@gmail.com>
2025-04-15 11:23:05 +0200

    Update waves of legends.py

--- waves of legends.py
+++ waves of legends.py
@@ -13,7 +13,7 @@ class character:
         '''character creator'''
         self.name = name
         self.health = health
-        self.damage = damage
+        self.damage = damagea
         self.mana = mana
 
         self.update_basic_health = health
@@ -38,35 +38,40 @@ class character:
 
 
     def mana_and_health_regen(self, my_health, my_mana):
-        self.health += ((my_health - self.health) * 0.1)
-        self.mana += ((my_mana - self.mana) * 0.31)
+        self.health += ((my_health - self.health) * 0.12)
+        self.mana += ((my_mana - self.mana) * 0.32)
 
 
     def basic_attack(self,enemy_damage):
-        self.health -= (enemy_damage*1.1)
+        self.health -= (enemy_damage)
 
 
     def ability(self,enemy_damage):
-        self.health -= (enemy_damage*1.4)
+        self.health -= (enemy_damage*1.5)
 
 
     def ult(self,enemy_damage):
-        self.health -= (enemy_damage*1.9)
+        self.health -= (enemy_damage*2.1)
 
 
     def on_level_stats_growth(self):
-       self.update_basic_health *= 1.43
-       self.update_basic_damage *= 1.34
-       self.update_basic_mana *= 1.32
+       self.update_basic_health *= 1.4
+       self.update_basic_damage *= 1.35
+       self.update_basic_mana *= 1.33
 
 #________________________________________________________________________________________________________________________________________________________________________________
 
-sett = character('Sett:', 100, 20, 70)
-viego = character('Viego:',90, 25, 80)
+sett = character('Sett:', 100, 22, 70)
+viego = character('Viego:',90, 24, 75)
 sylas = character('Sylas:',70, 30, 90)
-samira = character('Samira:', 85, 25, 85)
-thresh = character('Thresh:', 120, 15, 75)
+samira = character('Samira:', 80, 26, 80)
+thresh = character('Thresh:', 120, 16, 80)
 
+sleep(1)
+
+for i in "\nGame: Waves of Legends\n":
+    print(i,flush=True,end='')
+    sleep(0.06)
 
 sleep(1)
 pick_list = ['Sett','Viego','Sylas','Samira','Thresh']
@@ -74,30 +79,30 @@ pick_list_with_stats = [sett.stats(),viego.stats(),sylas.stats(),samira.stats(),
 character_list = [sett,viego,sylas,samira,thresh]
 
 
-for i in '\nChoose your summoner to fight as: ':
-    print(i,end='',flush=True)
-    sleep(0.06)
+while True:
+    
+    summoner = input('\nChoose your summoner to fight as: ').title().strip()
 
+    if summoner in pick_list:
+        for i in (f'\nYou have chosen: {pick_list[pick_list.index(summoner)]}'):
+            print(i,end='',flush=True)
+            sleep(0.06)
+        break
+    else:
+        continue
+        
 
-summoner = input().title().strip()
-
-
-if summoner in pick_list:
-    for i in (f'\nYou have chosen: {pick_list[pick_list.index(summoner)]}'):
-        print(i,end='',flush=True)
-        sleep(0.06)
-
-    summoner = pick_list.index(summoner)
-    summoner = character_list[summoner]
+summoner = pick_list.index(summoner)
+summoner = character_list[summoner]
 
 
 def fight(name,ability_cost,ult_cost):
     global deaths
 
     sleep(1)
-    print('\nPrepare for your Enemy!')
+    print('\n\nPrepare for your Enemy!')
     sleep(1.5)
-    print(f'\nAbility cost: {ability_cost}\nUlt cost: {ult_cost}')
+    print(f'\nAbility cost: {ability_cost} mana\nUlt cost: {ult_cost} mana')
     sleep(2)
     
     enemy = character(name, health_for_bot, damage_for_bot, mana_for_bot)
@@ -113,16 +118,29 @@ def fight(name,ability_cost,ult_cost):
 
 
     while True:
+        
+        while True:
 
-        if summoner.mana > ult_cost:
-            type_of_attack = input(f'\nChoose your attack: ([B]Basic Attack, [A]Ability Attack -{ability_cost} mana, [U]Ult -{ult_cost} mana): ').title().strip()
-            
-        elif summoner.mana < ability_cost:
-            type_of_attack = input('\nChoose your attack: ([B]Basic Attack): ').title().strip()
+            if summoner.mana >= ult_cost:
+                type_of_attack = input(f'\nChoose your attack: ([B]Basic Attack, [A]Ability Attack -{ability_cost} mana, [U]Ult -{ult_cost} mana): ').title().strip()
+                if type_of_attack == 'A' or type_of_attack == 'B' or type_of_attack == 'U':
+                    break
+                else:
+                    continue
 
-        elif summoner.mana < ult_cost:
-            type_of_attack = input(f'\nChoose your attack: ([B]Basic Attack, [A]Ability Attack -{ability_cost} mana): ').title().strip()
+            elif summoner.mana < ability_cost:
+                type_of_attack = input('\nChoose your attack: ([B]Basic Attack): ').title().strip()
+                if type_of_attack == 'B':
+                    break
+                else:
+                    continue
 
+            elif summoner.mana < ult_cost:
+                type_of_attack = input(f'\nChoose your attack: ([B]Basic Attack, [A]Ability Attack -{ability_cost} mana): ').title().strip()
+                if type_of_attack == 'A' or type_of_attack == 'B':
+                    break
+                else:
+                    continue
 
         if type_of_attack == 'B':
             enemy.basic_attack(summoner.damage)
@@ -130,12 +148,14 @@ def fight(name,ability_cost,ult_cost):
             sleep(0.5)
 
         elif type_of_attack == 'A':
+
             enemy.ability(summoner.damage)
             summoner.mana = summoner.mana - ability_cost
             print(f'\nYou have chosen: Ability -{ability_cost} mana')
             sleep(0.5)
 
         elif type_of_attack == 'U':
+
             enemy.ult(summoner.damage)
             summoner.mana = summoner.mana - ult_cost
             print(f'\nYou have chosen: Ult -{ult_cost} mana')
@@ -148,9 +168,27 @@ def fight(name,ability_cost,ult_cost):
             print("\nCongratulations! You have defeated your enemy!")
             break
         
-        enemy_attack = randint(1,3)
+        enemy_attack = randint(1,2)
+        first_attack = randint(1,2)
 
         sleep(1)
+
+        if first_attack == 1:
+
+            if enemy.mana >= ult_cost:
+                summoner.ult(enemy.damage)
+                enemy.mana = enemy.mana - ult_cost
+                print(f'\n{name} has chosen: Ult -{ult_cost} mana')
+                sleep(0.5)
+
+        elif first_attack == 2:
+
+            if enemy.mana >= ult_cost:
+                summoner.ability(enemy.damage)
+                enemy.mana = enemy.mana - ability_cost
+                print(f'\n{name} has chosen: Ability -{ability_cost} mana')
+                sleep(0.5)
+
 
         if enemy_attack == 1:
 
@@ -169,25 +207,6 @@ def fight(name,ability_cost,ult_cost):
                 summoner.ability(enemy.damage)
                 enemy.mana = enemy.mana - ability_cost
                 print(f'\n{name} has chosen: Ability -{ability_cost} mana')
-                sleep(0.5)
-
-        elif enemy_attack == 3:
-
-            if ability_cost < enemy.mana < ult_cost :
-                summoner.ability(enemy.damage)
-                enemy.mana = enemy.mana - ability_cost
-                print(f'\n{name} has chosen: Ability -{ability_cost} mana')
-                sleep(0.5)
-
-            elif enemy.mana > ult_cost:
-                summoner.ult(enemy.damage)
-                enemy.mana = enemy.mana - ult_cost
-                print(f'\n{name} has chosen: Ult -{ult_cost} mana')
-                sleep(0.5)
-
-            else:
-                summoner.basic_attack(enemy.damage)
-                print(f'\n{name} has chosen: Basic Attack')
                 sleep(0.5)
 
         enemy.stats()
@@ -214,7 +233,7 @@ def fight(name,ability_cost,ult_cost):
         enemy.stats()
         summoner.stats()
 
-fight('Teemo',35,55)
+fight('Teemo',35,60)
 
 
 def after_game_1():
@@ -257,6 +276,11 @@ fight('Twitch',65,95)
 after_game_1()
 on_level_bot_stats_growth()
 fight('Illaoi',85,125)
+
+
+sleep(1)
+print('Final Enemy!')
+sleep(1)
 
 
 after_game_1()
